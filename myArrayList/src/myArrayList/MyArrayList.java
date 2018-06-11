c8c37d76590c61bd19c11c90a8f5fb7f6f560b22
ShekharShelar <sshelar1@binghamton.edu>
2018-06-11 01:01:06 -0400

    I have implemented the myarraylist class and tested the code with some values. done insertion and deletion of numbers manually

--- myArrayList/src/myArrayList/MyArrayList.java
+++ myArrayList/src/myArrayList/MyArrayList.java
@@ -2,4 +2,85 @@ package myArrayList;
 
 public class MyArrayList{
 
+	private int[] myarray;
+	private int size;
+	
+	public MyArrayList() {
+		myarray = new int [50];
+		size=0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int sum() {
+		int sum=0;
+		for (int i = 0; i < size; i++) {
+			sum = sum + myarray[i];
+		}
+		return sum;
+	}
+	
+	public void insertSorted(int newValue) {
+		if (size==0) {
+			myarray[size] = newValue;
+			size++;
+			return;
+		}
+		int x = size-1;
+		while (x >= 0 && myarray[x] > newValue ) {
+			myarray[x+1] = myarray[x];
+			x--;
+		}
+        myarray[x+1] = newValue;	
+        size++;
+        if (size==myarray.length) {
+			resize();
+		}
+	}
+	
+	private void resize() {
+		// TODO Auto-generated method stub
+		int newSize = (int) (size + Math.floor(size/2));
+		int[] newMyArray = new int [newSize];
+		for (int i = 0; i < size; i++) {
+			newMyArray[i] = myarray[i];
+		}
+		myarray = newMyArray;
+	}
+
+	public void removeValue(int value) {
+		int pos;
+		while ((pos = indexOf(value)) != -1) {
+			//System.out.println("Position : " + pos);
+			for (int i = pos; i < size; i++) {
+				myarray[i] = myarray[i+1];
+			}
+			myarray[size-1] = 0;
+			size--;
+		}
+	}
+	
+	public int indexOf(int value) {
+		// TODO Auto-generated method stub
+		int pos = -1;
+		for (int i = 0; i < size; i++) {
+			if (myarray[i] == value) {
+				pos = i;
+				break;
+			}
+		}
+		return pos;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			builder.append(myarray[i] + " ");
+		}
+		return builder.toString();
+	}
 }
--- myArrayList/src/myArrayList/driver/Driver.java
+++ myArrayList/src/myArrayList/driver/Driver.java
@@ -1,5 +1,27 @@
 package myArrayList.driver;
 
+import myArrayList.MyArrayList;
+
 public class Driver{
 
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		MyArrayList myArrayList = new MyArrayList();
+		System.out.println("Array : " + myArrayList.toString());
+		System.out.println("Size : " + myArrayList.size());
+		System.out.println("Sum : " + myArrayList.sum());
+		myArrayList.insertSorted(10);
+		myArrayList.insertSorted(5);
+		myArrayList.insertSorted(7);
+		myArrayList.insertSorted(7);
+		System.out.println("Array : " + myArrayList.toString());
+		System.out.println("Size : " + myArrayList.size());
+		System.out.println("Sum : " + myArrayList.sum());
+		myArrayList.removeValue(7);
+		System.out.println("Array : " + myArrayList.toString());
+		System.out.println("Size : " + myArrayList.size());
+		System.out.println("Sum : " + myArrayList.sum());
+		System.out.println("Bye");
+	}
+	
 }
